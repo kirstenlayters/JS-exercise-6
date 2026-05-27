3e488914622cdebeba0167eb6e29360405bf465d
kirstenlayters <kirsten.layters@gmail.com>
2026-05-27 12:21:39 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(greet("Kirsten"));
+console.log(add(5, 10));
+
+//person i worked with is Muhammad Qasim Moos
